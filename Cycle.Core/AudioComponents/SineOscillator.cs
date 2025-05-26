2dbdf1eff654b04b361583a5e56da8422eee4c53
Rob <rob@bigdevelopments.co.uk>
2025-05-26 21:05:43 +0100

    missed file

--- Cycle.Core/AudioComponents/SineOscillator.cs
+++ Cycle.Core/AudioComponents/SineOscillator.cs
@@ -28,7 +28,7 @@ public class SineOscillator : Component
 		// default, but slow
 		//_output.Value = new Vector2(_amplitude.Value.X * MathF.Sin(_phase * 2 * MathF.PI), _amplitude.Value.Y * MathF.Sin(_phase * 2 * MathF.PI));
 
-		// this sin approximation is about 3x faster on i9-13900K
+		// this sin approximation is about 3x faster on i9-13900K, but likely depends on CPU cache state - we'll see
 		_output.Value = new Vector2(_amplitude.Value.X * Maths.Sin(_phase), _amplitude.Value.Y * Maths.Sin(_phase));
 	}
 }
